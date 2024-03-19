e342297a186e609d76f7c3eecaca20ed9ba1a364
Wuffluv <rodion.rycha@yandex.ru>
2024-03-19 20:04:06 +0900

    Сделал адекватный нижний бар

---
+++ 3lab/app/src/main/java/com/example/a3lab/FivethFragment.java
@@ -0,0 +1,36 @@
+package com.example.a3lab;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FivethFragment extends Fragment {
+
+    private FivethViewModel mViewModel;
+
+    public static FivethFragment newInstance() {
+        return new FivethFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fiveth, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(FivethViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ 3lab/app/src/main/java/com/example/a3lab/FivethViewModel.java
@@ -0,0 +1,7 @@
+package com.example.a3lab;
+
+import androidx.lifecycle.ViewModel;
+
+public class FivethViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
